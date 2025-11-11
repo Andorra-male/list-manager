90c5823d28c23a40bebec64b8a3d1e67b65c75bd
Andrij Lubkovych <andriyl262@gmail.com>
2025-11-11 22:48:28 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+def add_item(lst, item):
+    lst.append(item)
+
+def remove_item(lst, item):
+    if item in lst:
+        lst.remove(item)
+
+def print_list(lst):
+    for item in lst:
+        print(item)
